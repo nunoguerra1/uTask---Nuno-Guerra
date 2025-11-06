802b944e562d5acfd302fc5c775072994843909d
Nuno Guerra <nunomiguel@alunos.utfpr.edu.br>
2025-11-06 16:39:58 -0300

    feat: adicionado imagem de divisor e imagem de cadastro (ambos estilizados)

---
+++ src/components/ImagemCadastro.tsx
@@ -0,0 +1,12 @@
+import './ImagemCadastro.css'
+import IlustracaoCadastro from '../assets/imagens/IlustraçãoCadastro.png'
+
+function ImagemCadastro() {
+    return (
+        <div className='ImagemCadastro'>
+            <img src={IlustracaoCadastro} alt="imagemcadastro" />
+        </div>
+    )
+}
+
+export default ImagemCadastro
--- src/pages/Cadastro/Cadastro.tsx
+++ src/pages/Cadastro/Cadastro.tsx
@@ -1,12 +1,16 @@
 import './Cadastro.css'
 import BannerSuperior from '../../components/BannerSuperior'
 import BoxCadastro from '../../components/BoxCadastro'
+import Divisor from "../../components/Divisor";
+import ImagemCadastro from '../../components/ImagemCadastro'
 
 function Cadastro() {
     return (
         <div>
             <BannerSuperior />
             <BoxCadastro />
+            <Divisor />
+            <ImagemCadastro />
         </div>
     )
 }
