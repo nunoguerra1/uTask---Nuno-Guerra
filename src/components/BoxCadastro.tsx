ffc684665907072489173d0037e51a5df83c715c
Nuno Guerra <nunomiguel@alunos.utfpr.edu.br>
2025-11-07 18:53:49 -0300

    feat: adicionado modal de "Cadastrado com sucesso" estilizado

--- src/components/BoxCadastro.tsx
+++ src/components/BoxCadastro.tsx
@@ -3,6 +3,7 @@ import DivisorCadastro from '../assets/imagens/DivisorTelaLogin.png'
 import { useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
+import AlertCadastro from './AlertCadastro';
 
 interface ValidationErrors {
     usuario?: string;
@@ -20,6 +21,7 @@ function BoxCadastro() {
     })
 
     const [errors, setErrors] = useState<ValidationErrors>({})
+    const [showModal, setShowModal] = useState(false)
     const navigate = useNavigate()
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -35,49 +37,58 @@ function BoxCadastro() {
         if (Object.keys(validationErrors).length === 0) {
             axios.post('http://localhost:3000/users', formData)
                 .then(() => {
-                    alert("Registrado com sucesso!")
-                    navigate('/')
+                    setShowModal(true)
+                    setTimeout(() => {
+                        setShowModal(false)
+                        navigate('/')
+                    }, 3000)
                 })
                 .catch(err => console.log(err))
         }
     }
 
     return (
-        <form className='TelaCadastro' onSubmit={handleSubmit}>
 
-            <h1>uTask 3.0</h1>
+        <>
 
-            <img src={DivisorCadastro} alt="divisorCadastro" />
+            <form className='TelaCadastro' onSubmit={handleSubmit}>
 
-            <p>Crie uma conta</p>
+                <h1>uTask 3.0</h1>
 
-            <div className='formscad'>
-                <label htmlFor="usuario">Nome de usuário</label>
-                <input type="text" id='usuario' placeholder='Seu nome de usuário' onChange={(event) => setFormData({ ...formData, usuario: event.target.value })} />
-                {errors.usuario && <span className="error-message">{errors.usuario}</span>}
-            </div>
+                <img src={DivisorCadastro} alt="divisorCadastro" />
 
-            <div className='formscad'>
-                <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" placeholder='Endereço de e-mail' onChange={(event) => setFormData({ ...formData, email: event.target.value })} />
-                {errors.email && <span className="error-message">{errors.email}</span>}
-            </div>
+                <p>Crie uma conta</p>
 
-            <div className='formscad'>
-                <label htmlFor="senha">Senha</label>
-                <input type="password" id='senha' placeholder='Senha secreta' className={errors.csenha ? 'input-error' : ''} onChange={(event) => setFormData({ ...formData, senha: event.target.value })} />
-            </div>
+                <div className='formscad'>
+                    <label htmlFor="usuario">Nome de usuário</label>
+                    <input type="text" id='usuario' placeholder='Seu nome de usuário' onChange={(event) => setFormData({ ...formData, usuario: event.target.value })} />
+                </div>
 
-            <div className='formscad'>
-                <label htmlFor="confirmaSenha">Confirme a senha</label>
-                <input type="password" id='confirmaSenha' placeholder='Senha secreta' className={errors.csenha ? 'input-error' : ''} onChange={(event) => setFormData({ ...formData, csenha: event.target.value })} />
-                {errors.csenha && <span className="error-message">{errors.csenha}</span>}
-            </div>
+                <div className='formscad'>
+                    <label htmlFor="email">E-mail</label>
+                    <input type="email" id="email" placeholder='Endereço de e-mail' onChange={(event) => setFormData({ ...formData, email: event.target.value })} />
+                </div>
 
-            <button type='submit' className='btnCadastrar'>
-                Criar Cadastro
-            </button>
-        </form>
+                <div className='formscad'>
+                    <label htmlFor="senha">Senha</label>
+                    <input type="password" id='senha' placeholder='Senha secreta' className={errors.csenha ? 'input-error' : ''} onChange={(event) => setFormData({ ...formData, senha: event.target.value })} />
+                </div>
+
+                <div className='formscad'>
+                    <label htmlFor="confirmaSenha">Confirme a senha</label>
+                    <input type="password" id='confirmaSenha' placeholder='Senha secreta' className={errors.csenha ? 'input-error' : ''} onChange={(event) => setFormData({ ...formData, csenha: event.target.value })} />
+                    {errors.csenha && <span className="error-message">{errors.csenha}</span>}
+                </div>
+
+                <button type='submit' className='btnCadastrar'>
+                    Criar Cadastro
+                </button>
+            </form>
+
+            <AlertCadastro isOpen={showModal} />
+
+        </>
+
     )
 }
 
