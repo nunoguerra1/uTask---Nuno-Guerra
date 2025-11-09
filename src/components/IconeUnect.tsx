4c2eb6912e9dae90ca4c7a3534a7586dbe728cac
Nuno Guerra <nunomiguel@alunos.utfpr.edu.br>
2025-11-09 16:14:53 -0300

    feat: adicionado o icone da unect junto com o h1 no banner do dashboard, ambos estilizados

---
+++ src/components/IconeUnect.tsx
@@ -0,0 +1,12 @@
+import './IconeUnect.css'
+import icone from '../assets/icons/Logobranca.png'
+
+function IconeUnect() {
+    return (
+        <div className='UnectIcone'>
+            <img src={icone} alt="" />
+        </div>
+    )
+}
+
+export default IconeUnect
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,13 @@
+import './Dashboard.css'
+import BannerSuperior from '../../components/BannerSuperior'
+import IconeUnect from '../../components/IconeUnect'
+
 function Dashboard() {
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className='dashboard'>
+            <BannerSuperior />
+            <IconeUnect />
+            <h1>uTask 3.0</h1>
         </div>
     )
 }
