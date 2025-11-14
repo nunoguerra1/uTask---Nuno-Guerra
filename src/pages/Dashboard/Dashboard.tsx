741fae61c473c25159543804d9c2a28c00fc9b76
Nuno Guerra <nunomiguel@alunos.utfpr.edu.br>
2025-11-14 16:28:40 -0300

    feat: adicionado o box da frase do dia com ambas as api's, light e dark mode / adicionado o quadro kanban na tela do dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -2,6 +2,8 @@ import './Dashboard.css'
 import BannerSuperior from '../../components/BannerSuperior'
 import IconeUnect from '../../components/IconeUnect'
 import BannerInferior from '../../components/BannerInferior'
+import { QuadroKanban } from '../../components/QuadroKanban'
+import FraseDia from '../../components/FraseDia'
 
 function Dashboard() {
     return (
@@ -9,6 +11,8 @@ function Dashboard() {
             <BannerSuperior />
             <IconeUnect />
             <h1>uTask 3.0</h1>
+            <FraseDia />
+            <QuadroKanban />
             <BannerInferior />
         </div>
     )
